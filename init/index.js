55b7d389704b5cfb86357472abf256d57702985f
Sanvi Aswal <aswal.sanvi8@gmail.com>
2026-05-11 21:53:39 +0530

    edited

--- init/index.js
+++ init/index.js
@@ -1,23 +1,29 @@
-const mongoose=require("mongoose");
-const initData=require("./data.js");
-const Listing=require("../models/listing.js");
-const MONGO_URL="mongodb://127.0.0.1:27017/HavenHub";
+require("dotenv").config({ path: require("path").join(__dirname, "../.env") });
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = process.env.ATLASDB_URL; 
+
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
+
 main()
   .then(() => console.log("DB CONNECTED"))
   .catch((err) => console.log(err));
-  const initDB = async () => {
-    await Listing.deleteMany({});
 
-    const updatedData = initData.data.map((obj) => ({
-        ...obj,
-        owner: new mongoose.Types.ObjectId("69e34eb2853ca611d6f2f084"),
-    }));
+const initDB = async () => {
+  await Listing.deleteMany({});
 
-    await Listing.insertMany(updatedData);
+  const updatedData = initData.data.map((obj) => ({
+    ...obj,
+    owner: new mongoose.Types.ObjectId("69e34eb2853ca611d6f2f084"),
+  }));
 
-    console.log("data was initialized");
+  await Listing.insertMany(updatedData);
+  console.log("data was initialized");
+  mongoose.connection.close(); 
 };
+
 initDB();
